e812fabed6e983b09487195507e8df846452c93c
Ioan-Mihail Stan <stanioanmihail@gmail.com>
2015-04-24 20:08:06 +0300

    Hardcoded piechart added in Dashboard template

--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 def dashboard(request):
-	return render(request, 'profile/dashboard.html', {})
+    template = loader.get_template('profile/dashboard.html')
+    dashboard_dict = {
+        'HTTP (non-video)' : 200,
+        'VoIP' : 5,
+        'BitTorrent' : 400,
+        'Video' : 105,
+        'FTP' : 3,
+    
+    }
+    context = RequestContext(request, {
+        'dashboard_dict':dashboard_dict,
+    })
+    return HttpResponse(template.render(context))
+    #	return render(request, 'profile/dashboard.html', {})
 def stats(request):
 	return render(request, 'profile/advanced_statistics.html', {})
 def manage(request):
