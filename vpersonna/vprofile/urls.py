0008735b38cfeb0910574d2072b7b4d87489fc10
Ioan-Mihail Stan <stanioanmihail@gmail.com>
2015-04-24 17:14:53 +0300

    User profile template and structure

--- vpersonna/vprofile/urls.py
+++ vpersonna/vprofile/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.user_profile),
+    url(r'^$', views.dashboard, name="dashboard"),
+    url(r'^stats$', views.stats, name="stats"),
+    url(r'^manage$', views.manage, name="manage"),
+    url(r'^offers$', views.offers, name="offers"),
+    url(r'^transactions$', views.transactions, name="transactions"),
 ]
 
--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def user_profile(request):
-	return render(request, 'profile/user_profile.html', {})
+def dashboard(request):
+	return render(request, 'profile/dashboard.html', {})
+def stats(request):
+	return render(request, 'profile/advanced_statistics.html', {})
+def manage(request):
+	return render(request, 'profile/resources_mng.html', {})
+def offers(request):
+	return render(request, 'profile/offers.html', {})
+def transactions(request):
+	return render(request, 'profile/transaction_hist.html', {})
