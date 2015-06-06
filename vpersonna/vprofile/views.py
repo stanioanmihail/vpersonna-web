0ad583416f3f3fa656e3cc2ff56d1590ef8b0782
Ioan-Mihail Stan <stanioanmihail@gmail.com>
2015-06-06 23:51:28 +0300

    Solved statistics issues: legend desync, range function is not inclusive

--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -19,7 +19,6 @@ def home(request):
         'client_list' : client_list,
     }
     return HttpResponse(template.render(context))
-    #return redirect('/dashboard')
     
 def dashboard(request, client_id):
     template = loader.get_template('profile/dashboard.html')
@@ -78,10 +77,10 @@ def dashboard(request, client_id):
 
 
     context = RequestContext(request, {
-        'dashboard_dict':dashboard_dict,
-        'dashboard_dict_m1':dashboard_dict_m1,
-        'dashboard_dict_m2':dashboard_dict_m2,
-        'dashboard_dict_m3':dashboard_dict_m3,
+        'dashboard_dict':OrderedDict(sorted(dashboard_dict.items(), key = lambda t: t[0])),
+        'dashboard_dict_m1':OrderedDict(sorted(dashboard_dict_m1.items(), key = lambda t: t[0])),
+        'dashboard_dict_m2':OrderedDict(sorted(dashboard_dict_m2.items(), key = lambda t: t[0])),
+        'dashboard_dict_m3':OrderedDict(sorted(dashboard_dict_m3.items(), key = lambda t: t[0])),
         'top_rate_sites_matrix':top_rate_sites_matrix,
         'client_id': client_id,
         'client': client,
@@ -95,7 +94,6 @@ def dashboard(request, client_id):
         
     })
     return HttpResponse(template.render(context))
-    #	return render(request, 'profile/dashboard.html', {})
 
 def stats(request, client_id):
     #today - need change
@@ -112,7 +110,6 @@ def stats(request, client_id):
 
 def stats_date(request, client_id):
 
-    #today - need change
     date_format = '%d-%m-%Y'
     
     client = Client.objects.get(id = client_id)
@@ -120,37 +117,11 @@ def stats_date(request, client_id):
     start_date = datetime.datetime.strptime(request.POST['datepicker-start'], date_format)
     end_date = datetime.datetime.strptime(request.POST['datepicker-end'], date_format)
     
-    #tag_list = [ 'VoIP', 'HTTP (non-video)', 'BitTorent', 'Video' ]
-    tag_list = ServiceType.objects.all();
+    tag_list = ServiceType.objects.all().order_by('service_name');
 
-    #traffic_per_timeslot = {
-    #        '08-10': {
-    #            'VoIP': 10, 'HTTP (non-video)': 20, 'BitTorrent': 30, 'Video': 15
-    #            },
-    #        '10-12': {
-    #            'VoIP': 10, 'HTTP (non-video)': 20, 'BitTorrent': 30, 'Video': 15
-    #            },
-    #        '12-14': {
-    #            'VoIP': 5, 'HTTP (non-video)': 2, 'BitTorrent': 20, 'Video': 5
-    #            },
-    #        '14-16': {
-    #            'VoIP': 1, 'HTTP (non-video)': 0, 'BitTorrent': 0, 'Video': 1
-    #            },
-    #        '16-18': {
-    #            'VoIP': 3, 'HTTP (non-video)': 4, 'BitTorrent': 4, 'Video': 3
-    #            },
-    #        '18-20': {
-    #            'VoIP': 5, 'HTTP (non-video)': 2, 'BitTorrent': 17, 'Video': 20
-    #            },
-    #        '20-22': {
-    #            'VoIP': 5, 'HTTP (non-video)': 10, 'BitTorrent': 30, 'Video': 30
-    #            },
-    #        '22-24': {
-    #            'VoIP': 10, 'HTTP (non-video)': 5, 'BitTorrent': 10, 'Video': 15
-    #            },
-    #        }
-
+    next_day = end_date + datetime.timedelta(days=1) # range is not inclusive so it necessary the date of the next day
     traffic_per_timeslot = {} 
+
     for i in range(0, 23):
         crt_hours_range_dict = {}
         for s in tag_list:
@@ -158,7 +129,7 @@ def stats_date(request, client_id):
                 #data range
                 crt_service_accesses = ServiceUtilizationStatistics.objects.filter(
                                                         date__range=[start_date.strftime("%Y-%m-%d"), 
-                                                                    end_date.strftime("%Y-%m-%d")],
+                                                                     next_day.strftime("%Y-%m-%d")], #range is not inclusive
                                                         client = client_id,
                                                         service = s,
                                                         date__hour=i, 
@@ -172,14 +143,17 @@ def stats_date(request, client_id):
                                                         date__day = start_date.day,
                                                         date__hour = i,
                                                         client = client_id,
-                                                        service = s).aggregate(Sum('num_accesses', default=0))
+                                                        service = s,).aggregate(Sum('num_accesses', default=0))
+            else:
+                crt_service_accesses = []
 
             if crt_service_accesses.values()[0] != None: #no criteria match, no traffic stats stored 
                 crt_hours_range_dict[s.service_name] = crt_service_accesses.values()[0]
             else:
                 crt_hours_range_dict[s.service_name] = 0
 
-        traffic_per_timeslot[str(i).zfill(2) + ":00" +"-" + str(i).zfill(2) + ":59"] = crt_hours_range_dict
+        traffic_per_timeslot[str(i).zfill(2) + ":00" +"-" + str(i).zfill(2) + ":59"] = OrderedDict(sorted(crt_hours_range_dict.items(),
+                                                                                                          key = lambda t: t[0]))
 
     context = RequestContext(request, {
         'traffic_per_timeslot': OrderedDict(sorted(traffic_per_timeslot.items(), key=lambda t: t[0])),
@@ -192,7 +166,6 @@ def stats_date(request, client_id):
     })
 
     return HttpResponse(template.render(context))
-	#return render(request, 'profile/stats_by_date.html', {})
 
 def compute_total_bandwidth(client_id):
     client = Client.objects.get(id = client_id)
