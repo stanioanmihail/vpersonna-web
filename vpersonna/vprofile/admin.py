9dec7d53c4b90f97ca3ebeec3aa9e39a2a1c62c5
Ioan Mihail Stan <stanioanmihail@gmail.com>
2015-06-10 19:33:25 +0300

    Admin interface complete for brute packet observation

--- vpersonna/vprofile/admin.py
+++ vpersonna/vprofile/admin.py
@@ -5,7 +5,7 @@ class ServiceUtilizationStatisticsModel(admin.ModelAdmin):
     list_display = ("client", "service", "date", "num_accesses")
 
 class BrutePacketModel(admin.ModelAdmin):
-    list_display = ("id", "ip_src", "port_src", "ip_dst", "port_dst", "timestamp", 'get_transport_protocol')
+    list_display = ("id", "ip_src", "port_src", "ip_dst", "port_dst", 'host_address' , 'get_transport_protocol', 'traffic_type', 'timestamp' )
 class IPAllocationModel(admin.ModelAdmin):
     list_display = ("client", "ip_addr")
 # Register your models here.
